3d158cc88c038a34f98c5ba9f43a0d0db9f50921
WingSMC <gergely089@gmail.com>
2022-11-08 00:41:15 +0100

    pl6+

--- prolog/khf6.pl
+++ prolog/khf6.pl
@@ -1,2 +1,36 @@
-osszeg_szukites(Trees, oszl(Col, S), DirsIn, DirsOut) :- !.
-osszeg_szukites(Trees, sor(Row, S), DirsIn, DirsOut) :- !.
+osszeg_szukites(Trees, oszl(Col, S), DirsIn, DirsOut) :-
+	!,
+	zip(Trees, DirsIn, Zip),
+	W is Col - 1,
+	E is Col + 1.
+	
+
+osszeg_szukites(Trees, sor(Row, S), DirsIn, DirsOut) :-
+	!,
+	zip(Trees, DirsIn, Zip).
+
+
+zip([], [], []) :- !.
+zip([Y|Ys], [X|Xs], [X-Y|Zs]) :-
+	zip(Ys, Xs, Zs).
+
+
+col_map([], _, S, []) :-
+	!,
+	S =< 0.
+col_map([TY-TX-DirsIn|Is], Col, Sum, [Ds|DsOutRest]) :-
+	(
+		TX < Col   -> (
+			member(e, DirsIn),
+			S1 is Sum - 1
+		);
+		TX > Col   ->  (
+			member(w, DirsIn),
+			S1 is Sum - 1
+		);
+		TX =:= Col ->  (
+			(member(n, DirsIn);
+			member(s, DirsIn)),
+			S1 is Sum - 1
+		)
+	).
