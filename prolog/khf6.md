11989f1b7d3769dbcf9e048ab2b6f66543af84a5
WingSMC <gergely089@gmail.com>
2022-11-08 18:52:51 +0100

    ++khf6

--- prolog/khf6.md
+++ prolog/khf6.md
@@ -1,10 +1,19 @@
 # KHF6
+
 ## Test
+
 ```prolog
 osszeg_szukites([6-6,9-7], oszl(7,2), [[n,s,w],[e,n,s,w]], []).
 osszeg_szukites([6-6,9-7], oszl(7,2), [[e,n,s,w],[e,n,s,w]], [[e],[n,s]]).
 osszeg_szukites([6-6,9-7], oszl(7,2), [[e,n,s,w],[n,s]], [[e],[n,s]]).
 osszeg_szukites([6-6,9-7], oszl(7,1), [[e,n,s,w],[n,s]], [[n,s,w],[n,s]]).
 osszeg_szukites([6-6,9-7], oszl(7,0), [[e,n,s,w],[n,s]], []).
+\+ osszeg_szukites([6-6,9-7], oszl(7,1), [[e,n,s,w],[e,n,s,w]], ILs).
+
+osszeg_szukites([6-6,9-7], oszl(7,2), [[n,s,w],[e,n,s,w]], ILs).
+osszeg_szukites([6-6,9-7], oszl(7,2), [[e,n,s,w],[e,n,s,w]], ILs).
+osszeg_szukites([6-6,9-7], oszl(7,2), [[e,n,s,w],[n,s]], ILs).
+osszeg_szukites([6-6,9-7], oszl(7,1), [[e,n,s,w],[n,s]], ILs).
+osszeg_szukites([6-6,9-7], oszl(7,0), [[e,n,s,w],[n,s]], ILs).
 \+ osszeg_szukites([6-6,9-7], oszl(7,1), [[e,n,s,w],[e,n,s,w]], ILs).
 ```
--- prolog/khf6.pl
+++ prolog/khf6.pl
@@ -1,36 +1,73 @@
-osszeg_szukites(Trees, oszl(Col, S), DirsIn, DirsOut) :-
+osszeg_szukites(Trees, oszl(Col, NeededSum), DirsIn, DirsOut) :-
 	!,
 	zip(Trees, DirsIn, Zip),
-	W is Col - 1,
-	E is Col + 1.
-	
-
-osszeg_szukites(Trees, sor(Row, S), DirsIn, DirsOut) :-
-	!,
-	zip(Trees, DirsIn, Zip).
-
-
-zip([], [], []) :- !.
-zip([Y|Ys], [X|Xs], [X-Y|Zs]) :-
-	zip(Ys, Xs, Zs).
-
-
-col_map([], _, S, []) :-
-	!,
-	S =< 0.
-col_map([TY-TX-DirsIn|Is], Col, Sum, [Ds|DsOutRest]) :-
+	dir_map(col, Zip, Col, ForcedZip, FreeZip, UselessZip),
+	length(ForcedZip, LFo),
 	(
-		TX < Col   -> (
-			member(e, DirsIn),
-			S1 is Sum - 1
-		);
-		TX > Col   ->  (
-			member(w, DirsIn),
-			S1 is Sum - 1
-		);
-		TX =:= Col ->  (
-			(member(n, DirsIn);
-			member(s, DirsIn)),
-			S1 is Sum - 1
+		(LFo  > NeededSum ) -> (DirsOut = []);
+		%(LFo =:= NeededSum) -> (
+		%	contstrain free trees (remove ones inside)
+		%);
+		(
+			length(FreeZip, LFr),
+			MaxSum is LFo + LFr,
+			(
+				(MaxSum < NeededSum) -> (DirsOut = []);
+				%(MaxSum = NeededSum) -> (
+				%	constrain free trees (remove ones outside)
+				%);
+				DirsOut = [ForcedZip, FreeZip, UselessZip]
+			)
 		)
 	).
+
+%osszeg_szukites(Trees, sor(Row, NeededSum), DirsIn, DirsOut) :-
+%	!,
+%	zip(Trees, DirsIn, Zip).
+
+
+dir(col, low, [e], [n,s,w]) :- !.
+dir(col, mid, [n,s], [e,w]) :- !.
+dir(col, high, [w], [e,n,s]) :- !.
+
+dir(row, low, [s], [e,n,w]) :- !.
+dir(row, mid, [e,w], [n,s]) :- !.
+dir(row, high, [n], [e,s,w]) :- !.
+
+sel(col, _-TX, TX) :- !.
+sel(row, TY-_, TY) :- !.
+
+dir_map(_, [], _, [], [], []) :- !.
+dir_map(Dir, [TY-TX-DirsIn|Is], Coord, ForcedTrees, FreeTrees, UselessTrees) :-
+	dir_map(Dir, Is, Coord, FoTs, FrTs, UTs),
+	sel(Dir, TY-TX, C),
+	(
+		(C  <  Coord) -> (dir(Dir, low,  Need, NotNeed));
+		(C =:= Coord) -> (dir(Dir, mid,  Need, NotNeed));
+		(C  >  Coord) -> (dir(Dir, high, Need, NotNeed))
+	),
+	(contains_any(DirsIn, Need) -> (
+		UselessTrees = UTs,
+		(contains_any(DirsIn, NotNeed) -> (
+			ForcedTrees = FoTs,
+			FreeTrees = [TY-TX-DirsIn|FrTs]
+		);(
+			ForcedTrees = [TY-TX-DirsIn|FoTs],
+			FreeTrees = FrTs
+		))
+	);(
+		ForcedTrees = FoTs,
+		FreeTrees = FrTs,
+		UselessTrees = [TY-TX-DirsIn|UTs]
+	)).
+
+contains_any([], []) :- !.
+contains_any([X|Xs], Ys) :-
+	notrace,
+	member(X, Ys) ->
+		true;
+		contains_any(Xs, Ys).
+
+zip([], [], []) :- !.
+zip([Y|Ys], [X|Xs], [Y-X|Zs]) :-
+	zip(Ys, Xs, Zs).
